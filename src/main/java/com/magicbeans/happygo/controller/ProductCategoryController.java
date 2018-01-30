0c12cdf30fccb7f86735f03c9122d3c23a2f8960
kalen <kalen>
2018-01-30 18:07:36 +0800

    商品、商品分类

--- src/main/java/com/magicbeans/happygo/controller/ProductCategoryController.java
+++ src/main/java/com/magicbeans/happygo/controller/ProductCategoryController.java
@@ -1,21 +1,12 @@
 package com.magicbeans.happygo.controller;
 
-import com.magicbeans.base.Pages;
 import com.magicbeans.base.ajax.ResponseData;
-import com.magicbeans.base.db.Filter;
-import com.magicbeans.base.db.Order;
-import com.magicbeans.happygo.Message;
 import com.magicbeans.happygo.controller.base.BaseController;
 import com.magicbeans.happygo.entity.ProductCategory;
 import com.magicbeans.happygo.service.IProductCategoryService;
 import com.magicbeans.happygo.util.StatusConstant;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
