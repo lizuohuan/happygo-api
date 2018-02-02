2a3c7059de0a8ca16dc5fb0eb14f1688bdd2353f
Eric <mostlove_xf@outlook.com>
2018-02-02 11:45:59 +0800

    功能新增

--- src/main/java/com/magicbeans/happygo/controller/ShopCarController.java
+++ src/main/java/com/magicbeans/happygo/controller/ShopCarController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import com.magicbeans.happygo.service.IShopCarService;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -37,71 +39,91 @@ public class ShopCarController extends BaseController {
 
 
     @Resource
-    private  IShopCarService shopCarService;
+    private IShopCarService shopCarService;
     @Resource
     private RedisService redisService;
 
 
-    @ApiOperation(value = "添加购物车")
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @ApiOperation(value = "购物车产品操作")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "productId",value = "产品ID",required = true),
-            @ApiImplicitParam(name = "number",value = "产品ID,一般情况下固定值：1，如果该参数值 大于1，则认定该产品的总数量为该值" +
-                    " 而不是增加该值",required = true)
+            @ApiImplicitParam(name = "productId", value = "产品ID", required = true),
+            @ApiImplicitParam(name = "isAdd", value = "0:减  1:加", required = true),
+            @ApiImplicitParam(name = "number", value = "产品ID,一般情况下固定值：1，如果该参数值 大于1，则认定该产品的总数量为该值" +
+                    " 而不是增加该值", required = true)
     })
-    public ResponseData add(ShopCar shopcar){
-        if(CommonUtil.isEmpty(shopcar.getNumber(),shopcar.getProductId())){
-            return buildFailureJson(StatusConstant.FIELD_NOT_NULL,"参数不能为空");
+    public ResponseData add(ShopCar shopcar, Integer isAdd) {
+        if (CommonUtil.isEmpty(shopcar.getNumber(), shopcar.getProductId(), isAdd)) {
+            return buildFailureJson(StatusConstant.FIELD_NOT_NULL, "参数不能为空");
         }
-        if(shopcar.getNumber() <= 0){
-            return buildFailureJson(StatusConstant.ARGUMENTS_EXCEPTION,"参数错误");
+        if (shopcar.getNumber() <= 0) {
+            return buildFailureJson(StatusConstant.ARGUMENTS_EXCEPTION, "参数错误");
         }
         try {
             User currentUser = LoginHelper.getCurrentUser(redisService);
             ShopCar car = shopCarService.getShopCarByUser(currentUser.getId(), shopcar.getProductId());
-            if(null == car){
+            if (null == car) {
                 shopCarService.save(shopcar);
-            }
-            else{
-                car.setNumber(shopcar.getNumber() > 1 ? shopcar.getNumber() : (car.getNumber() + shopcar.getNumber()));
+            } else {
+                int num = 0;
+                if (shopcar.getNumber() == 1) {
+                    num = isAdd == 0 ? -1 : 1;
+                }
+                car.setNumber(shopcar.getNumber() > 1 ? shopcar.getNumber() : (car.getNumber() + num));
                 shopCarService.update(car);
             }
         } catch (InterfaceCommonException e) {
-            return buildFailureJson(e.getErrorCode(),e.getMessage());
-        }catch (Exception e) {
-            logger.error(e.getMessage(),e);
-            return buildFailureJson(StatusConstant.Fail_CODE,"操作失败");
+            return buildFailureJson(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return buildFailureJson(StatusConstant.Fail_CODE, "操作失败");
         }
-        return buildSuccessCodeJson(StatusConstant.SUCCESS_CODE,"操作成功");
+        return buildSuccessCodeJson(StatusConstant.SUCCESS_CODE, "操作成功");
     }
 
 
-    @RequestMapping(value = "/del",method = RequestMethod.POST)
-    @ApiOperation(value = "减除")
-    public ResponseData del(String id){
-            if(CommonUtil.isEmpty(id)){
-                return buildFailureJson(StatusConstant.FIELD_NOT_NULL,"参数不能为空");
+    @RequestMapping(value = "/del", method = RequestMethod.POST)
+    @ApiOperation(value = "购物车删除商品")
+    @ApiImplicitParam(name = "ids",value = "购物车ID集合，多个以逗号隔开")
+    public ResponseData del(String ids) {
+        if (CommonUtil.isEmpty(ids)) {
+            return buildFailureJson(StatusConstant.FIELD_NOT_NULL, "参数不能为空");
+        }
+        try {
+            LoginHelper.getCurrentUser(redisService);
+            List<String> idList = new ArrayList<>();
+            String[] split = ids.split(",");
+            for (int i = 0; i < split.length; i++) {
+                idList.add(split[i]);
             }
-            try {
-                LoginHelper.getCurrentUser(redisService);
-                ShopCar shop = shopCarService.find(id);
-                if(null != shop ){
-                    if(shop.getNumber() == 1){
-                        shopCarService.delete(id);
-                    }
-                    else{
-                        shop.setNumber(shop.getNumber() - 1);
-                        shopCarService.update(shop);
-                    }
-                }
-            } catch (InterfaceCommonException e) {
-                return buildFailureJson(e.getErrorCode(),e.getMessage());
-            }catch (Exception e) {
-                logger.error(e.getMessage(),e);
-                return buildFailureJson(StatusConstant.Fail_CODE,"操作失败");
-            }
-            return buildSuccessCodeJson(StatusConstant.SUCCESS_CODE,"操作成功");
+            shopCarService.del(idList);
+        } catch (InterfaceCommonException e) {
+            return buildFailureJson(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return buildFailureJson(StatusConstant.Fail_CODE, "操作失败");
+        }
+        return buildSuccessCodeJson(StatusConstant.SUCCESS_CODE, "操作成功");
     }
+
+
+
+    @RequestMapping(value = "/getShopCar", method = RequestMethod.POST)
+    @ApiOperation(value = "获取购物车")
+    public ResponseData getShopCar() {
+        try {
+            User currentUser = LoginHelper.getCurrentUser(redisService);
+            return buildSuccessJson(StatusConstant.SUCCESS_CODE, "操作成功",
+                    shopCarService.getShopCar(currentUser.getId()));
+        } catch (InterfaceCommonException e) {
+            return buildFailureJson(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return buildFailureJson(StatusConstant.Fail_CODE, "操作失败");
+        }
+
+    }
+
 
 
 
